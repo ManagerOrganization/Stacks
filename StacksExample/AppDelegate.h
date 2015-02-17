834c73cefd42899286c8ae96b3e5c4084fa75424
dozel <dcozel@gmail.com>
2015-02-17 12:45:17 -0800

    Initial Commit

---
+++ StacksExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StacksExample
+//
+//  Created by Devin Ozel on 2/17/15.
+//  Copyright (c) 2015 knotch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StacksExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StacksExample
+//
+//  Created by Devin Ozel on 2/17/15.
+//  Copyright (c) 2015 knotch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
