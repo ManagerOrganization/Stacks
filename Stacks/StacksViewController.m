0b3be2f4c945d7295929f67abbb75a89826bb673
dozel <dcozel@gmail.com>
2015-03-16 14:09:26 -0700

    License everywhere

--- Stacks/StacksViewController.m
+++ Stacks/StacksViewController.m
@@ -2,9 +2,24 @@
 //  StacksViewController.m
 //  StacksOnStacks
 //
-//  Created by Knotch on 3/17/14.
-//  Copyright (c) 2014 knotch. All rights reserved.
+//  Copyright © 2014, 2015 Knotch, Inc.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+//  Software, and to permit persons to whom the Software is furnished to do so, subject
+//  to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #define animationTime 0.2f
 #import "StacksViewController.h"
@@ -66,14 +81,14 @@
     CGFloat diffX = translation - initialX;
     if (diffX < 0.0f && translation != 0.0f) {
         _scrollDirection = ScrollDirectionNext;
-        _currentSnapshot = [[UIImageView alloc] initWithImage:[self.view makeImageFromCurrentView]];
+        _currentSnapshot = [[UIImageView alloc] initWithImage:[self.view imageFromView]];
         [_currentSnapshot setFrame:self.view.bounds];
         [self.view addSubview:_currentSnapshot];
     }
     else if(diffX > 0.0f && translation != 0.0f) { //CHECK
         _scrollDirection = ScrollDirectionPrev;
         if (_currentPage > 0) {
-            _currentSnapshot = [[UIImageView alloc] initWithImage:[self.view makeImageFromCurrentView]];
+            _currentSnapshot = [[UIImageView alloc] initWithImage:[self.view imageFromView]];
             [_currentSnapshot setFrame:self.view.bounds];
             [self.view addSubview:_currentSnapshot];
             _currentPage--;
--- Stacks/UIView+Snapshot.m
+++ Stacks/UIView+Snapshot.m
@@ -2,9 +2,24 @@
 //  UIView+Snapshot.m
 //  StacksOnStacks
 //
-//  Created by Knotch on 3/17/14.
-//  Copyright (c) 2014 knotch. All rights reserved.
+//  Copyright © 2014, 2015 Knotch, Inc.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+//  Software, and to permit persons to whom the Software is furnished to do so, subject
+//  to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import "UIView+Snapshot.h"
 
@@ -12,24 +27,19 @@
 
 #pragma mark - Screen Snapshot
 
-- (UIImage *)makeImageFromCurrentView {
-    UIGraphicsBeginImageContextWithOptions(self.frame.size, NO, 0);
+- (UIImage *)imageFromView {
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.opaque, [[UIScreen mainScreen] scale]);
     
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    
-    CGContextSaveGState(context);
-    CGContextTranslateCTM(context, self.center.x, self.center.y);
-    CGContextConcatCTM(context, self.transform);
-    CGContextTranslateCTM(context, -1 * self.frame.size.width * self.layer.anchorPoint.x, -1 * self.frame.size.height * self.layer.anchorPoint.y);
     if ([self respondsToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)]) {
         [self drawViewHierarchyInRect:self.bounds afterScreenUpdates:YES];
-    } else {
-        [self.layer renderInContext:context];
     }
-    CGContextRestoreGState(context);
+    else {
+        [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    }
     
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
+    
     return image;
 }
 
